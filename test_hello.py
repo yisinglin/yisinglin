373390ab8a2874e01da1ee080d32dd1f2ae50fc1
Ellie Lin <104173676@student.swin.edu.au>
2024-08-05 16:49:36 +1000

    test3

--- test_hello.py
+++ test_hello.py
@@ -1,19 +1,30 @@
 from hello import toyou, add, subtract
 
-
 def setup_function(function):
     print(f" Running Setup: {function.__name__}")
     function.x = 10
-
 
 def teardown_function(function):
     print(f" Running Teardown: {function.__name__}")
     del function.x
 
+def test_hello_toyou():
+    setup_function(test_hello_toyou)
+    assert toyou("test") == "hi test", "Failed test_hello_toyou"
+    teardown_function(test_hello_toyou)
 
-### Run to see failed test
-#def test_hello_add():
-#    assert add(test_hello_add.x) == 10
+def test_hello_add():
+    setup_function(test_hello_add)
+    assert add(test_hello_add.x) == 11, "Failed test_hello_add"
+    teardown_function(test_hello_add)
 
 def test_hello_subtract():
-    assert subtract(test_hello_subtract.x) == 9
+    setup_function(test_hello_subtract)
+    assert subtract(test_hello_subtract.x) == 9, "Failed test_hello_subtract"
+    teardown_function(test_hello_subtract)
+
+# Run the tests
+test_hello_toyou()
+test_hello_add()
+test_hello_subtract()
+
