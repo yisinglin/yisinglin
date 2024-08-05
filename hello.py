5e1591dcd5ef9068d4ef9c02b47ea31c5cd2257e
Ellie Lin <104173676@student.swin.edu.au>
2024-08-05 16:54:47 +1000

    add description

--- hello.py
+++ hello.py
@@ -1,13 +1,39 @@
-#var=1
-#var=var
+"""
+This module contains simple functions for demonstration purposes.
+"""
 
 def toyou(x):
+    """
+    Returns a greeting string.
+    
+    Args:
+    x (str): The name to greet.
+    
+    Returns:
+    str: A greeting string.
+    """
     return f"hi {x}"
 
-
 def add(x):
+    """
+    Adds 1 to the input number.
+    
+    Args:
+    x (int): The number to be incremented.
+    
+    Returns:
+    int: The incremented number.
+    """
     return x + 1
 
-
 def subtract(x):
+    """
+    Subtracts 1 from the input number.
+    
+    Args:
+    x (int): The number to be decremented.
+    
+    Returns:
+    int: The decremented number.
+    """
     return x - 1
